63f68050a234585f0d75ae062055ae0c32868158
Subhadeep Datta <subhadeepdatta2@gmail.com>
2022-11-02 22:06:29 +0530

    sinnup Bug Resolved

--- lib/mutations.ts
+++ lib/mutations.ts
@@ -1,5 +1,7 @@
 import fetcher from './fetcher'
 
+
+
 export const auth = (
   mode: 'signin' | 'signup',
   body: { email: string; password: string }
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,49 +1,53 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
-import { NextApiRequest, NextApiResponse } from 'next'
+import {NextApiRequest, NextApiResponse} from 'next'
 import prisma from '../../lib/prisma'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const salt = bcrypt.genSaltSync()
-  const { email, password } = req.body
+    const salt = bcrypt.genSaltSync()
+    const {email, password, firstName, lastName} = req.body;
 
-  let user
 
-  try {
-    user = await prisma.user.create({
-      // @ts-ignore
-      data: {
-        email,
-        password: bcrypt.hashSync(password, salt),
-      },
-    })
-  } catch (e) {
-    res.status(401)
-    res.json({ error: 'User already exists' })
-    return
-  }
+    let user
 
-  const token = jwt.sign(
-    {
-      email: user.email,
-      id: user.id,
-      time: Date.now(),
-    },
-    'hello',
-    { expiresIn: '8h' }
-  )
 
-  res.setHeader(
-    'Set-Cookie',
-    cookie.serialize('TRAX_ACCESS_TOKEN', token, {
-      httpOnly: true,
-      maxAge: 8 * 60 * 60,
-      path: '/',
-      sameSite: 'lax',
-      secure: process.env.NODE_ENV === 'production',
-    })
-  )
+    try {
+        user = await prisma.user.create({
+            data: {
+                email: email,
+                password: bcrypt.hashSync(password, salt),
+                firstName: firstName,
+                lastName: lastName
+            }
+        })
+    } catch (error) {
+        res.status(403)
+        res.json({error: 'Email already exists'})
+        return
+    }
 
-  res.json(user)
+    const token = jwt.sign(
+        {
+            email: user.email,
+            id: user.id,
+            time: Date.now(),
+        },
+        'hello',
+        {expiresIn: '8h'}
+    )
+
+    res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('TRAX_ACCESS_TOKEN', token, {
+            httpOnly: true,
+            maxAge: 8 * 60 * 60,
+            path: '/',
+            sameSite: 'lax',
+            secure: process.env.NODE_ENV === 'production',
+        })
+    )
+
+    res.status(200)
+    res.json(user)
 }

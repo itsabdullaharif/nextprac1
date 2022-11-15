c42af0324935246d4f18acf87b0598dd9425fbdd
abdullah <abdullaharif059@gmail.com>
2022-11-16 04:18:04 +0530

    changes

--- lib/index.tsx
+++ lib/index.tsx
@@ -3,7 +3,6 @@ import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
 import { useMe } from './hooks'
 import prisma from './prisma'
-import img1 from '../public/banner12.jpg'
 
 const Home = ({ artists }) => {
   const { user } = useMe()
@@ -15,7 +14,7 @@ const Home = ({ artists }) => {
       subtitle="profile"
       title={`${user?.firstName} ${user?.lastName}`}
       description={`${user?.playlistsCount} public playlists`}
-      image={img1}
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
     >
       <Box color="white" paddingX="40px">
         <Box marginBottom="40px">
@@ -29,7 +28,7 @@ const Home = ({ artists }) => {
             <Box paddingX="10px" width="20%">
               <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
                 <Image
-                  src="https://picsum.photos/300/300"
+                  src="https://placekitten.com/300/300"
                   borderRadius="100%"
                 />
                 <Box marginTop="20px">
